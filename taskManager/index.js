58cf51342cda9a608c9225f9978a63c580bdafc8
Julia <ibralenko6724809@gmail.com>
2023-07-16 00:38:29 +0300

    add a function that removes an attribute disables

--- taskManager/index.js
+++ taskManager/index.js
@@ -6,7 +6,7 @@ const form = document.forms.taskDesk;
 const hiddenEl = document.querySelector('.grey')
 const myNodelist = document.getElementsByTagName("li");
 const todoView = document.getElementById('todoView')
-const checkbox = document.querySelector('.checkbox')
+const checkbox = document.querySelectorAll('checkbox')
 const toDolist =[];
 
 function addTask() {
@@ -35,60 +35,16 @@ function showTask() {
 showTask()
 
 
+function removeAttrDis () {
+    if (localStorage.getItem('tasks') !== null){
+        return clearBtn.removeAttribute('disabled')
+    }
+}
+
 function clearTasks(){
     localStorage.removeItem('tasks');
     tasksContainer.textContent = '';
     hiddenEl.classList.remove('hidden');
 }
 
-function isChecked (){
-    if (checkbox.checked){
-        return
-    }
-}
-
-// function clearAll (){
-//     if (localStorage.getItem('tasks') != undefined){
-
-//     }
-// }
-
-/*document.querySelector('.add-btn').addEventListener('click', function () {
-    if (toDolist.filter(t => t.title === inputTask.value).length > 0) {
-        return error.textContent = 'Такая задача уже есть'
-    } else if (inputTask.value.length === 0) {
-        return error.textContent = 'Название не может быть пустым'
-    } else {
-        const id = toDolist.length + 1
-        toDolist.push({
-            id,
-            title: inputTask.value,
-            complete: false
-        })
-
-        const li = document.createElement('li')
-        const span = document.createElement('span')
-        span.innerText = inputTask.value
-        span.dataset.inputTask = id
-
-        const checkbox = document.createElement('input')
-        checkbox.type = 'checkbox'
-        checkbox.inputTask = id
-
-        checkbox.addEventListener('change', function (e) {
-            const inputTask = toDolist.forEach(t => {
-                if (t.id === e.target.inputTask) {
-                    const span = document.querySelector(`span[data-inputTask='${e.target.inputTask}']`)
-                    t.complete = !t.complete
-                    span && t.complete ? span.style.textDecoration = 'line-through' : span.style.textDecoration = 'none'
-                }
-            })
-        })
-        li.appendChild(checkbox)
-        li.appendChild(span)
-        todoView.appendChild(li)
-        inputTask.value = ''
-    }
-
-})
-*/
+removeAttrDis ()
